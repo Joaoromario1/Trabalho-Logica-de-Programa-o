77c46169fb260d799a185fdb8ede509106ccdaf4
Joaoromario1 <152514502+Joaoromario1@users.noreply.github.com>
2023-12-05 11:29:05 -0300

    Update questão 1.py

--- Python/questão 1.py
+++ Python/questão 1.py
@@ -1,1 +1,7 @@
-feifb
+vp = int(input("Digite a velocidade maxima da avenidade: "))
+vm = int(input("Digite a velocidade do motorista: "))
+if vp < vm:
+    multa = (vm - vp) * 5 
+    print(f'A multa é de: {multa} reais') 
+elif vm <= vp:
+    print("Não há multa")
