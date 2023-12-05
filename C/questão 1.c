5b5726233ebcbbc0a4797f2a4bf270c9ac12edf2
Joaoromario1 <152514502+Joaoromario1@users.noreply.github.com>
2023-12-05 11:37:12 -0300

    Update questão 1.c

--- C/questão 1.c
+++ C/questão 1.c
@@ -1,2 +1,19 @@
-swd
-fr45hth
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int vp, vm, multa;
+    int taxa = 5;
+
+    printf("Digite a velocidade permitida: ");
+    scanf("%d", &vp);
+    printf("Digite a velocidade do motorista: ");
+    scanf("%d", &vm);
+    multa = (vm - vp) * taxa;
+    if(vm > vp){
+        printf("Você recebeu uma multa de: %d reais", multa);
+
+    }else{
+        printf("Não há multa!");
+    }
+}
