68220556448f2496abde4c720c753ee74690f795
Joaoromario1 <152514502+Joaoromario1@users.noreply.github.com>
2023-12-05 11:22:21 -0300

    Update questão 1.java

--- Java/questão 1.java
+++ Java/questão 1.java
@@ -1,1 +1,31 @@
-w
+package trabalhojava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class TrabalhoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite a velocidade permitida:");
+        int vp = entrada.nextInt();
+        System.out.println("Digite a velocidade do motorista:");
+        int vm = entrada.nextInt();
+        int multa;
+        int taxa = 5;
+        if(vm > vp){
+            multa = (vm - vp)* taxa;
+            System.out.println("A multa foi: "+multa);
+        }else{
+            System.out.println("Você não foi multado!!");
+        }
+    }
+    
+}
