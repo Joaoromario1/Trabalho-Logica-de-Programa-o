f484e8a1ba96f17ea8fba6598ec04260996a1899
Joaoromario1 <152514502+Joaoromario1@users.noreply.github.com>
2023-12-05 11:23:35 -0300

    Update questão 2.java

--- Java/questão 2.java
+++ Java/questão 2.java
@@ -1,1 +1,41 @@
-ebd
+package trabalhojava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class TrabalhoNota {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a primeira nota:");
+        int nota1 = entrada.nextInt();
+        
+        System.out.println("Digite a segunda nota:");
+        int nota2 = entrada.nextInt();
+         
+        int media = (nota1 + nota2)/2;
+        if(media >= 70){
+            System.out.println("Aprovado!!");
+        }else if(media >= 40 && media < 69){
+            System.out.println("Exame final!!");
+            System.out.println("Digite a nota do exame final:");
+            int notaexame = entrada.nextInt();
+            if(media + notaexame >= 100){
+                System.out.println("Aprovado");
+            }else{
+                System.out.println("Reprovado!!");
+            }
+        }if(media<40){
+            System.out.println("Reprovado!!");
+        }
+                
+        
+    }
+    
+}
